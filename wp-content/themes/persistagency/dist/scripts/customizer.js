c7b0b34dfdcf002c233b83145224c69e76884885
Joseph Chambers <joseph@michael-chambers.com>
2021-01-12 22:18:35 -0600

    Add vendor and compiled assets to git
    /vendor
    /dist

---
+++ wp-content/themes/persistagency/dist/scripts/customizer.js
@@ -0,0 +1,43 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["/scripts/customizer"],{
+
+/***/ "./resources/assets/scripts/customizer.js":
+/*!************************************************!*\
+  !*** ./resources/assets/scripts/customizer.js ***!
+  \************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+/**
+ * This file allows you to add functionality to the Theme Customizer
+ * live preview. jQuery is readily available.
+ *
+ * {@link https://codex.wordpress.org/Theme_Customization_API}
+ */
+
+/**
+ * Change the blog name value.
+ *
+ * @param {string} value
+ */
+wp.customize('blogname', function (value) {
+  value.bind(function (to) {
+    return $('.brand').text(to);
+  });
+});
+
+/***/ }),
+
+/***/ 1:
+/*!******************************************************!*\
+  !*** multi ./resources/assets/scripts/customizer.js ***!
+  \******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /Users/lalo/repos/persist/public/wp-content/themes/persistagency/resources/assets/scripts/customizer.js */"./resources/assets/scripts/customizer.js");
+
+
+/***/ })
+
+},[[1,"/scripts/manifest"]]]);
+//# sourceMappingURL=customizer.js.map
