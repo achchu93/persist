5e649102b0fdbf848d288a3b97e2f4c6aa92930f
Matt <matt@stiltsdesigns.com>
2021-03-09 15:46:10 -0800

    animations demo

--- wp-content/themes/persist/animations/index.js
+++ wp-content/themes/persist/animations/index.js
@@ -1,7 +1,7 @@
 const homeAnimation = bodymovin.loadAnimation( {
 	container: document.getElementById( 'home_marquee' ),
 	renderer: 'svg',
-	loop: true,
+	loop: false,
 	autoplay: true,
 	setSpeed: 1,
 	path: obj.templateUrl + '/animations/home/data.json',
