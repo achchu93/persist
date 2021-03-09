b2fd6ed950c09b73198b5f75b9ec8d608c011f29
Matt <matt@stiltsdesigns.com>
2021-03-09 13:17:03 -0800

    removing old theme

--- wp-content/themes/persist/animations/index.js
+++
@@ -1,7 +0,0 @@
-const homeAnimation = bodymovin.loadAnimation({
-	container: document.getElementById('home_marquee'),
-	renderer: 'svg',
-	loop: false,
-	autoplay: true,
-	path: obj.templateUrl+'/animations/home/data.json'
-})
