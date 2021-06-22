b35105630000254dddc64ade153c372376457126
Justin Rogers <justinrogersj@gmail.com>
2021-06-23 03:15:56 +0530

    UNIVERSAL - Navigation scroll back

--- wp-content/themes/persistagency/dist/scripts/app.js
+++ wp-content/themes/persistagency/dist/scripts/app.js
@@ -74,6 +74,33 @@ __webpack_require__.r(__webpack_exports__);
     }, function () {
       $(this).find('.navigation__submenu').hide().removeClass('translate-y-0 scale-100 opacity-100').addClass('-translate-y-3 scale-95 opacity-0');
     });
+    var scrolled = false;
+    var previousScroll = 0;
+    var header = $('#pageHeader');
+    var headerOffset = header.offset().top + header.height();
+    $(window).on('scroll', function () {
+      scrolled = true;
+    });
+    setInterval(function () {
+      if (scrolled) {
+        var currentScroll = $(window).scrollTop();
+
+        if (Math.abs(previousScroll - currentScroll) <= 5) {
+          return;
+        }
+
+        console.log(currentScroll, headerOffset);
+
+        if (currentScroll > previousScroll && currentScroll > headerOffset || currentScroll < headerOffset) {
+          header.removeClass('sticked-menu');
+        } else if (currentScroll + $(window).height() < $(document).height()) {
+          header.addClass('sticked-menu');
+        }
+
+        previousScroll = currentScroll;
+        scrolled = false;
+      }
+    }, 250);
   });
 });
 
