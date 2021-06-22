347598354e679ceb8e767af8e5178a178bb13985
Justin Rogers <justinrogersj@gmail.com>
2021-06-22 20:30:17 +0530

    symlink recreation

--- wp-content/themes/persistagency/storage/framework/views/099215a30d9ac0fea217b033afa9986385ff0b6b.php
+++
@@ -1,8 +0,0 @@
-<div class="<?php echo e($class); ?> block-big-image">
-
-  <?php if(!empty($image)): ?>
-   <?php echo wp_get_attachment_image($image['id'], 'full', false, ['class' => 'w-full h-auto']); ?> 
-  <?php endif; ?>
-
-</div> 
-<?php /**PATH C:\xampp\htdocs\persist\wp-content\themes\persistagency\resources\views/blocks/block-big-image.blade.php ENDPATH**/ ?>
--- wp-content/themes/persistagency/storage/framework/views/d02d507da41d2b1843d9c97d3bb0e07effd0333b.php
+++
@@ -1,3 +0,0 @@
-<div data-animation="<?php echo e($animation_folder_name); ?>" data-animationdelay="<?php echo e($animation_delay); ?>" class="<?php echo e($class); ?> ">
-	<div class="section-bg absolute w-full h-full object-cover"></div>
-</div><?php /**PATH C:\xampp\htdocs\persist\wp-content\themes\persistagency\resources\views/blocks/block-lottie.blade.php ENDPATH**/ ?>
