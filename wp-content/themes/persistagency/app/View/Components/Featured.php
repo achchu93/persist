4f9fa562bba937c24588f0960599a6da266fa4fb
Justin Rogers <justinrogersj@gmail.com>
2021-06-24 01:17:51 +0530

    CASE STUDIES - header video

--- wp-content/themes/persistagency/app/View/Components/Featured.php
+++ wp-content/themes/persistagency/app/View/Components/Featured.php
@@ -18,7 +18,7 @@ class Featured extends Component
     $video_url = esc_url( get_field('featured_video', $this->post->ID) );
     if( !empty( $video_url ) ){
       $thumbnails_url = has_post_thumbnail( $this->post ) ? get_the_post_thumbnail_url($this->post, 'full') : '';
-      $featured = '<video src="'.$video_url.'" poster="'.$thumbnails_url.'" autoplay class="w-full h-auto"></video>';
+      $featured = '<video src="'.$video_url.'" poster="'.$thumbnails_url.'" class="w-full h-auto" autoplay muted preload="metadata"></video>';
     }else{
       $featured = has_post_thumbnail( $this->post ) ? get_the_post_thumbnail($this->post, 'full', [ 'class' => 'w-full h-auto' ]) : '';
     }
